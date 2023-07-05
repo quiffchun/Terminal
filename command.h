543a48717367b5ca37fb6934ff9fe1c1693da716
Muyang Li <122759182+quiffchun@users.noreply.github.com>
2023-07-05 19:33:47 +0800

    Add files via upload

---
+++ command.h
@@ -0,0 +1,14 @@
+#ifndef COMMAND
+#define COMMAND 1 
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <stdlib.h>
+#include <map>
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <ntsecapi.h>
+#include <tlhelp32.h>
+#include<conio.h>
+using namespace std;
+#endif
